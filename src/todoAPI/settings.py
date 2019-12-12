2e20a3b39f27f44e9dcac1b42e2ba74cc4f64b0b
khvyu <khvyus@gmail.com>
2019-12-12 12:39:20 +0900

    Test

--- src/todoAPI/settings.py
+++ src/todoAPI/settings.py
@@ -8,10 +8,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'et4wie@55ymyngo4%3!$e4-a(vzq6@aa^+@6)sw2@)i$4sdm^)'
+# SECRET_KEY = 'et4wie@55ymyngo4%3!$e4-a(vzq6@aa^+@6)sw2@)i$4sdm^)'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'et4wie@55ymyngo4%3!$e4-a(vzq6@aa^+@6)sw2@)i$4sdm^)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
